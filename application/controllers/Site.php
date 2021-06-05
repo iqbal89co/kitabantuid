77caa51e8f177a76d1bdc162f15d9926ddf1fbef
iqbal89co <iqbalfakhriza89@gmail.com>
2021-06-05 22:01:36 +0700

    lengkapi login

--- application/controllers/Site.php
+++ application/controllers/Site.php
@@ -12,31 +12,37 @@ class Site extends CI_Controller
 
 	public function listBisnis()
 	{
+		$data['user'] = $this->user->getUser($this->session->userdata('email'));
 		$data['title'] = 'List Bisnis';
 		$this->view->getDefault($data, 'main/listBisnis');
 	}
 	public function caraInvest()
 	{
+		$data['user'] = $this->user->getUser($this->session->userdata('email'));
 		$data['title'] = 'Cara Investasi';
 		$this->view->getDefault($data, 'main/caraInvest');
 	}
 	public function pertanyaan()
 	{
+		$data['user'] = $this->user->getUser($this->session->userdata('email'));
 		$data['title'] = 'Pertanyaan';
 		$this->view->getDefault($data, 'main/pertanyaan');
 	}
 	public function tentangKami()
 	{
+		$data['user'] = $this->user->getUser($this->session->userdata('email'));
 		$data['title'] = 'Tentang Kami';
 		$this->view->getDefault($data, 'main/tentangKami');
 	}
 	public function daftarBisnis()
 	{
+		$data['user'] = $this->user->getUser($this->session->userdata('email'));
 		$data['title'] = 'Daftarkan Bisnis';
 		$this->view->getDefault($data, 'main/daftarBisnis');
 	}
 	public function detailBisnis()
 	{
+		$data['user'] = $this->user->getUser($this->session->userdata('email'));
 		$data['title'] = 'Detail Bisnis';
 		$this->view->getDefault($data, 'main/detailBisnis');
 	}
